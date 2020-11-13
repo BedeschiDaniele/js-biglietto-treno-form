2701bd932509fa48e179d744ed614da59770bb02
Daniele <daniele.bedeschi94@gmail.com>
2020-11-13 15:55:07 +0100

    commit js

--- js/script.js
+++ js/script.js
@@ -1,14 +1,22 @@
 // Il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
-var km = parseInt(prompt("Inserisci i chilometri da percorrere"));
-var eta = parseInt(prompt("Inserisci la tua eta'"));
+
+var generaBottone = document.getElementById("genera");
+generaBottone.addEventListener("click",
+ function() {
+   var numerokm = document.getElementById("km").value;
+   console.log(numerokm);
+   var eta = document.getElementById("fascia-eta").value;
+   console.log(eta);
+ }
+);
 // Controllo valore inserito
-if (isNaN(km)) {
-  alert("Hai inserito un valore sbagliato");
-}
-if ((km <= 0)) {
-  alert("Hai inserito un valore sbagliato");
-  km = 0;
-}
+// if (isNaN(km)) {
+//   alert("Hai inserito un valore sbagliato");
+// }
+// if ((km <= 0)) {
+//   alert("Hai inserito un valore sbagliato");
+//   km = 0;
+// }
 // il prezzo del biglietto è definito in base ai km (0.21 € al km);
 var prezzoBiglietto = km * 0.21;
 // va applicato uno sconto del 20% per i minorenni;
@@ -25,3 +33,12 @@ if (eta > 65) {
 document.getElementById('km').innerHTML=km;
 document.getElementById('eta').innerHTML=eta;
 document.getElementById('prezzo').innerHTML=prezzoBiglietto.toFixed(2);;
+
+
+var element = document.getElementById("saluto");
+
+element.addEventListener('click',
+ function() {
+ // codice
+ }
+);
